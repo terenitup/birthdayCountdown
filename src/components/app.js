ed5c47caca90956a7ada653753c50ee2bbd43693
terenitup <terentaniuchi@gmail.com>
2018-09-15 14:09:21 -0600

    added birthdayForm component with datepicker component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import BirthdayForm from './birthdayForm';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        
+        <BirthdayForm />
+
+        
       </div>
     );
   }
