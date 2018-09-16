aa9937f72b3ff00389fb7dbcc578ea3381fbe5e9
terenitup <terentaniuchi@gmail.com>
2018-09-15 18:11:23 -0600

    added js timer

--- src/components/clock.js
+++ src/components/clock.js
@@ -4,11 +4,13 @@ class Clock extends Component {
   
     constructor(props) {
         super(props)
-        
+
+        this.timer = 0
+        this.birthday = props.birthdayFormState.startDate.toString()
         this.getTimeRemaining = this.getTimeRemaining.bind(this);
 
         this.state = {
-            timeRemaining: this.getTimeRemaining(this.props.birthdayFormState.startDate.toString())
+            timeRemaining: this.getTimeRemaining(props.birthdayFormState.startDate.toString())
         }
     }
 
@@ -32,13 +34,15 @@ class Clock extends Component {
         }
     }
 
-    componentWillMount() {
-        
+    componentDidMount() {
+        this.timer = setInterval(() => {
+            const timeRemaining = this.getTimeRemaining(this.birthday)
+            this.setState({ timeRemaining: timeRemaining })
+        }, 1000);
     }
 
   render() {
     const data = this.state.timeRemaining
-    console.log("RENDERING")
 
     return (
       <div>
