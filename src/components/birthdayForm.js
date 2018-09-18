53cb5d646cb11343ef8eef97f07845a973c1eee4
terenitup <terentaniuchi@gmail.com>
2018-09-18 10:41:08 -0600

    added countdown clock styles and chagne date styles

--- src/components/birthdayForm.js
+++ src/components/birthdayForm.js
@@ -44,7 +44,11 @@ class BirthdayForm extends Component {
                 this.state.formCompleted ? 
                     <div>
                         <Clock birthdayFormState={this.state} />            
-                        <a onClick={this.handleChangeDate}>Change Date</a>
+                        <a 
+                            className='change-date'
+                            onClick={this.handleChangeDate}>
+                            Change Date
+                        </a>
                     </div>
                 :
                     <div>
