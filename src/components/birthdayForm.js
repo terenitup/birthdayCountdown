b9a4da55273a9be36b7c8fe174a52290b935e13b
terenitup <terentaniuchi@gmail.com>
2018-09-18 21:06:46 -0600

    added flexbox to birthdayForm component

--- src/components/birthdayForm.js
+++ src/components/birthdayForm.js
@@ -39,10 +39,10 @@ class BirthdayForm extends Component {
 
     render() {
         return (
-        <div>
+        <div className='birthday-container'>
             {
                 this.state.formCompleted ? 
-                    <div>
+                    <div className='clock-container'>
                         <Clock birthdayFormState={this.state} />            
                         <a 
                             className='change-date'
@@ -51,12 +51,14 @@ class BirthdayForm extends Component {
                         </a>
                     </div>
                 :
-                    <div>
-                        <DatePicker 
+                    <div className='date-picker-container'>
+                        <DatePicker className='date-picker'
                             selected={this.state.startDate}
                             onChange={this.handleChange}
-                        />    
-                        <a onClick={this.handleGenerate}>Generate Countdown</a>        
+                        />
+                        <div className='submit-container'>
+                            <a onClick={this.handleGenerate}>Generate Countdown</a>        
+                        </div>    
                     </div>
             }
         </div>
